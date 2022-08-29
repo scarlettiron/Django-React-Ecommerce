c6e8c4827d9cff2e3b1717b0ed0cc971ab926b03
scarlettiron <scottscarlett@gmail.com>
2022-08-29 00:25:10 -0400

    ignore certain files

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.db.models import Prefetch
+from .serializers import ProductList_Serializer
+from .models import Product
 
-# Create your views here.
+class product_list(generics.ListAPIView):
+    model = Product
+    serializer_class = ProductList_Serializer
+    queryset = Product.objects.prefetch_related(Prefetch('productpackage_set', to_attr = 'packages'))
