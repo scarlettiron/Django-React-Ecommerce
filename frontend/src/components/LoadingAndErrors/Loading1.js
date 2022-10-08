24a67a7d74eca592d38480a34cd8e6af35c8e097
scarlettiron <scottscarlett@gmail.com>
2022-10-07 21:18:21 -0400

    styled-loading-spinner

--- frontend/src/components/LoadingAndErrors/Loading1.js
+++ frontend/src/components/LoadingAndErrors/Loading1.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import '../../css/general.css'
+import Button1 from '../buttonsAndInputs/Button1'
+import '../../css/loader.css'
 
-const Loading1 = () => {
+const Loading1 = ({btnAction = null}) => {
   return (
-    <div>
-        <h1>I"m loading</h1>
+    <div className='lds-container'>
+      {btnAction && <><div className='lds-btn-wrapper'><Button1 action={btnAction} text={'cancel'}/></div></>}
+      <div className='lds-wrapper'>
+      <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      </div>
     </div>
   )
 }
