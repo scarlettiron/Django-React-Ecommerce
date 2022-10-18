d267acf5ffbd41b760fd54386d6e3d0d28126fc5
scarlettiron <scottscarlett@gmail.com>
2022-10-18 01:06:43 -0400

    restyled breakpoints for navbar

--- frontend/src/components/cards/CategoryCard.js
+++ frontend/src/components/cards/CategoryCard.js
@@ -6,16 +6,14 @@ import '../../css/containers-cards.css'
 const CategoryCard = ({category}) => {
   return (
     <div className='category-card'>
-        <Link to={`categories/${category.title}`} className='w-100 h-100 justify-content-center'>
         {category.placeholder ?
             <>
-              <img src={category.placeholder} alt={category.title} className='subcategory-card-img'/>
+              <img src={category.placeholder} alt={category.title}/>
               <h2 className='subcategory-card-title-absolute'>{category.title}</h2>
             </>
             :
-            <h2 className='subcategory-card-text'>{category.title}</h2>
+            <h2 className='margin-0 subcategory-card-text'>{category.title}</h2>
         }
-        </Link>
     </div>
   )
 }
